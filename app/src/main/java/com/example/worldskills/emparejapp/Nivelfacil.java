51b24083d4d17cd31f2691bac924a48d90073645
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-08-29 15:30:26 -0500

    fff

--- app/src/main/java/com/example/worldskills/emparejapp/Nivelfacil.java
+++ app/src/main/java/com/example/worldskills/emparejapp/Nivelfacil.java
@@ -1,11 +1,13 @@
 package com.example.worldskills.emparejapp;
 
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,6 +34,7 @@ public class Nivelfacil extends AppCompatActivity {
 
     int acierto;
 
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,19 +68,19 @@ public class Nivelfacil extends AppCompatActivity {
         ib0 = (ImageButton)findViewById(R.id.im1);
         botonera[0]= ib0;
         ib1 = (ImageButton)findViewById(R.id.im2);
-        botonera[1]= ib0;
+        botonera[1]= ib1;
         ib2 = (ImageButton)findViewById(R.id.im3);
-        botonera[2]= ib0;
+        botonera[2]= ib2;
         ib3 = (ImageButton)findViewById(R.id.im4);
-        botonera[3]= ib0;
+        botonera[3]= ib3;
         ib4 = (ImageButton)findViewById(R.id.im5);
-        botonera[4]= ib0;
+        botonera[4]= ib4;
         ib5 = (ImageButton)findViewById(R.id.im6);
-        botonera[5]= ib0;
+        botonera[5]= ib5;
         ib6 = (ImageButton)findViewById(R.id.im7);
-        botonera[6]= ib0;
+        botonera[6]= ib6;
         ib7 = (ImageButton)findViewById(R.id.im8);
-        botonera[7]= ib0;
+        botonera[7]= ib7;
 
     }
 
@@ -88,30 +91,36 @@ public class Nivelfacil extends AppCompatActivity {
             primera.setImageResource(imagenes[arraybarajado.get(i)]);
             primera.setEnabled(false);
             primerclick = arraybarajado.get(i);
-        }else{
+        }else {
             imb.setScaleType(ImageButton.ScaleType.CENTER_CROP);
             imb.setImageResource(imagenes[arraybarajado.get(i)]);
             imb.setEnabled(false);
             bloqueo = true;
             segundoclick = arraybarajado.get(i);
-            if (primerclick == segundoclick){
-                primera=null;
-                bloqueo=false;
+            if (primerclick == segundoclick) {
+                primera = null;
+                bloqueo = false;
                 acierto++;
-                if (acierto==5){
-
+                Toast.makeText(getApplicationContext(),"bien",Toast.LENGTH_LONG).show();
+                if (acierto == 5) {
+                    Toast.makeText(getApplicationContext(),"bien",Toast.LENGTH_LONG).show();
+                    /*mediaPlayer= new MediaPlayer();
+                    MediaPlayer.create(this,R.raw.win1);
+                    mediaPlayer.prepare(MediaPlayer);*/
                 }
-            }handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    primera.setScaleType(ImageButton.ScaleType.CENTER_CROP);
-                    primera.setImageResource(R.drawable.fondo);
-                    imb.setScaleType(ImageButton.ScaleType.CENTER_CROP);
-                    imb.setImageResource(R.drawable.fondo);
-                    primera.setEnabled(true);
-                    imb.setEnabled(true);
-                }
-            },1000);
+            } else {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        primera.setScaleType(ImageButton.ScaleType.CENTER_CROP);
+                        primera.setImageResource(R.drawable.fondo);
+                        imb.setScaleType(ImageButton.ScaleType.CENTER_CROP);
+                        imb.setImageResource(R.drawable.fondo);
+                        bloqueo=false;
+                        primera.setEnabled(true);
+                    }
+                }, 1000);
+            }
         }
     }
 
